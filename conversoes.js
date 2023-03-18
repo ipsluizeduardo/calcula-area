cb16ed881cb04bec96e8aae6e27b6a2d7bdda469
ipsluizeduardo <ipsluizeduardo@gmail.com>
2023-03-18 14:51:16 -0300

    teste1

---
+++ conversoes.js
@@ -0,0 +1,16 @@
+// tipo de dados 
+//booleanos
+
+// conversão implicita 
+
+const numero = 456;
+const numeroString = "456";
+
+
+// conversão  explicita
+
+//Number 
+//String
+
+
+console.log(numero === Number(numeroString));
